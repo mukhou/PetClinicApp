a164bac3f1dd0ec3734920ba6f2b7405bf821702
jhini <banerjeeurmi@gmail.com>
2020-02-10 18:57:39 -0500

    Adding swagger

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/config/SwaggerConfig.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/config/SwaggerConfig.java
@@ -6,10 +6,45 @@ Time: 4:09 PM
 
 package guru.springframework.sfgpetclinic.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
 public class SwaggerConfig {
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2).
+                select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/");
+    }
+
+    private ApiInfo metaData(){
+        Contact contact = new Contact("Urmi Mukhopadhyay", "https://mukhou", "banerjeeurmi@gmail.com");
+        return new ApiInfo(
+                "Spring Pet Clinic App",
+                "Spring Pet Clinic App",
+                "1.0",
+                "http.//dummy",
+                contact,
+                "l1",
+                "http://dummy",
+                new ArrayList<>());
+
+    }
+
 }
